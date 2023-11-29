0ace5c0e2b73768e005e53c9edc7f0c97f84b76c
RomanRudin <rudin.roman@bk.ru>
2023-11-29 11:59:24 +0200

    Updated

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from parser import Parser
 
 if __name__ == "__main__":
-    parser = Parser(file_name='data3')
-    parser.set_me('139-145-737 75')
+    parser = Parser(file_name='data0')
+    parser.set_me('') #TODO
     parser.set_place(21)
     answer = parser.start()
     for line in answer:
